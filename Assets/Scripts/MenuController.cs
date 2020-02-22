7396e77408857a43272a719516712fb88f50ef85
Vincent Lauro <vinniehat@gmail.com>
2020-02-21 20:47:19 -0500

    Added the disable music button Added Sound Effects on buttons Added New Game/Menu Music Added new Icon Assets

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button PlayNowButton;
+    public Button MusicButton;
+    public AudioSource MusicSource;
+    public AudioSource ButtonAudioSource;
+    public AudioClip ButtonAudioClip;
+
+	void Start()
+    {
+	    if (PlayerPrefs.GetInt("music") == 0)
+	    {
+		    MusicSource.mute = true;
+	    }
+	    
+	    PlayNowButton.onClick.AddListener(playNow);
+		MusicButton.onClick.AddListener(musicToggle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void playNow()
+    {
+	    SceneManager.LoadScene("Easy");
+    }
+
+    void musicToggle()
+    {
+	    ButtonAudioSource.PlayOneShot(ButtonAudioClip);
+	    if (PlayerPrefs.GetInt("music") == 1)
+	    {
+		    PlayerPrefs.SetInt("music", 0);
+		    PlayerPrefs.Save();
+		    MusicSource.mute = true;
+
+	    }
+	    else
+	    {
+		    PlayerPrefs.SetInt("music", 1);
+		    MusicSource.mute = false;
+	    }
+    }
+}
---
+++ Assets/Scripts/PlaySFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySFX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource GameAudioSource;
+    public AudioClip PassAudioClip;
+    public AudioClip FailAudioClip;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
